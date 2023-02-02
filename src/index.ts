d2b0633661053e2e72a9243039f5c804753741f0
Covadonga M <cova.millan@gmail.com>
2023-02-02 22:30:17 +0100

    Add titles component, css and test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { Header } from './components/header/header';
+import { Title } from './components/titles/titles';
 
 new Header('.container', 'My Series');
+new Title('.main', 'Series list', 'Pending series');
